828da0e415886c90057e919db67f34f76b3c5a2a
Feedbackhunter <aplicativo@feedbackhunter.com.br>
2016-01-05 11:47:45 -0200

    1.0

--- www/js/tools.js
+++ www/js/tools.js
@@ -1,10 +1,4 @@
-function onload(){
-	document.addEventListener('deviceready', checkConnection, false);
-}
 
 function checkConnection() {
     window.plugins.toast.show("haeiaehaeoihea", 'long', 'center');
-}
-function alertCallback(){
-
 }
