60b88e4128a23f3642cf96fa9d7020ed7a961461
Feedbackhunter <aplicativo@feedbackhunter.com.br>
2016-01-05 14:18:34 -0200

    1.0

--- www/js/tools.js
+++ www/js/tools.js
@@ -1,12 +1,8 @@
 function checkConnection() {
-    navigator.notification.confirm(
-    'You are the winner!', // message
-     onConfirm,            // callback to invoke with index of button pressed
-    'Game Over',           // title
-    ['Restart','Exit']     // buttonLabels
-    );
+    
+alert('haeheahue');
 }
 
-function onConfirm(){
-    
+function CallBackFunction() {
+    // do something
 }
