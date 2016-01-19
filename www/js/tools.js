d714082c571ed899976c30984cc0dd5e72e1bb0f
Feedbackhunter <aplicativo@feedbackhunter.com.br>
2016-01-19 16:20:33 -0200

    1.2
    Modificações no formulário WEB

--- www/js/tools.js
+++ www/js/tools.js
@@ -16,27 +16,11 @@ function checkConnection() {
 function aoFecharJanela(){
   alert("A janela foi fechada pelo usuário");
 }
-function abrirPopup(url, windowName, opts, callback) {
-    var popup = window.open("mailto:contato@feedbackhunter.com.brt?subject=subject of the email&body=whatever body body", windowName, opts);
-    var intervalo = setInterval(function() {
-        try {
-            if (popup == null || popup.closed) {
-                window.clearInterval(intervalo);
-                callback(popup);
-            }
-        }
-        catch (e) { }
-    }, 20000);
-    return popup;
-}
 function scannear(){
     alert('hue');
     cordova.plugins.barcodeScanner.scan(
       function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
+        window.open('http://feedbackhunter.com.br/formulario_novo/main.html'); 
       }, 
       function (error) {
           alert("Scanning failed: " + error);
